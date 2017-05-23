29a921aca13f6b6192a8517d7552caad9eae3a2d
James Lefty Larimer <James.Larimer@icstars-columbus.org>
2017-05-23 12:26:54 -0400

    https://github.com/jamesllarimer/RockPaperScissorsConsoleApp.git

--- RockPaperScissorsConsoleApp/Program.cs
+++ RockPaperScissorsConsoleApp/Program.cs
@@ -10,6 +10,70 @@ namespace RockPaperScissorsConsoleApp
     {
         static void Main(string[] args)
         {
-        }
+           
+                Console.WriteLine("Let's play Rock, paper, scissors?");
+                Console.Write("Type rock, paper, or scissors: ");
+                string userAnswer = Console.ReadLine();
+                string computerAnswer = "";
+                Random rnd = new Random();
+                int value = rnd.Next(0, 33);
+                //To make computer choose answer
+                if (value <= 11)
+                {
+                    computerAnswer = "rock";
+                }
+                else if (value <= 22)
+                {
+                    computerAnswer = "paper";
+                }
+                else
+                {
+                    computerAnswer = "scissors";
+                }
+                //user picks rock
+                if (userAnswer == "rock" && computerAnswer == "scissors")
+                {
+                    Console.WriteLine("You chose " + userAnswer + ", Bob chose " + computerAnswer + ". You Win!");
+                    Console.ReadLine();
+                }
+                else if (userAnswer == "rock" && computerAnswer == "paper")
+                {
+                    Console.WriteLine("You chose " + userAnswer + ", Bob chose " + computerAnswer + ". You lose!");
+                    Console.ReadLine();
+                }
+                //user picks paper
+                else if (userAnswer == "paper" && computerAnswer == "scissors")
+                {
+                    Console.WriteLine("You chose " + userAnswer + ", Bob chose " + computerAnswer + ". You lose!");
+                    Console.ReadLine();
+                }
+                else if (userAnswer == "paper" && computerAnswer == "rock")
+                {
+                    Console.WriteLine("You chose " + userAnswer + ", Bob chose " + computerAnswer + ". You win!");
+                    Console.ReadLine();
+                }
+                //user picks scissors
+                else if (userAnswer == "scissors" && computerAnswer == "rock")
+                {
+                    Console.WriteLine("You chose " + userAnswer + ", Bob chose " + computerAnswer + ". You lose!");
+                    Console.ReadLine();
+                }
+                else if (userAnswer == "scissors" && computerAnswer == "paper")
+                {
+                    Console.WriteLine("You chose " + userAnswer + ", Bob chose " + computerAnswer + ". You win!");
+                    Console.ReadLine();
+                }
+                //if it is a tie
+                else
+                {
+                    Console.WriteLine("You chose " + userAnswer + ", Bob chose " + computerAnswer + ". It is a tie!");
+                    Console.ReadLine();
+                }
+
+
+
+            }
+
+        
     }
 }
